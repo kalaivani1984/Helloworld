31626e90199fa15a061e68b179917bebed92895b
kalaivani1984 <kalainbs27@gmail.com>
2021-02-07 13:14:29 -0600

    this is my first project

---
+++ Helloworld/src/AreaOfrectangle.java
@@ -0,0 +1,12 @@
+
+public class AreaOfrectangle {
+
+	public static void main(String args[]) {
+		int width = 5;
+		int height = 10;
+		int area = width * height;
+		System.out.println("Area of rectangle=" + area);
+	}
+
+}
+
